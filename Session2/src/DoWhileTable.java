b25626b5cd397460412ca212b5703eca8bf55692
GSISP <pushpalatha.gadiraju@gmail.com>
2015-01-16 21:51:51 +0530

    ReTest Modify

--- Session2/src/DoWhileTable.java
+++ Session2/src/DoWhileTable.java
@@ -2,7 +2,7 @@ public class DoWhileTable {
    public static void main(String[] args) {       
       int no = Integer.parseInt(args[0]);  
   
-     // count from i = 1 to 10  
+     
       int i = 1;  
       do {  
          System.out.println(i*no);   
